65c06492114664d0e71a6f79abde0be1b7dbc77b
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-04-27 11:29:59 +0930

    Add files via upload

---
+++ Helicopter.cpp
@@ -0,0 +1,26 @@
+#include "Helicopter.h"
+
+Helicopter::Helicopter() {}
+
+Helicopter::Helicopter(int w, std::string n) : AirCraft(w), name(n) {}
+
+std::string Helicopter::get_name() const {
+    return name;
+}
+
+void Helicopter::set_name(const std::string& n) {
+    name = n;
+}
+
+void Helicopter::fly(int headwind, int minutes) {
+    float fuelConsumptionPerMinute = (headwind >= 40) ? 0.4 : 0.2;
+    if (get_weight() > 5670) {
+        fuelConsumptionPerMinute += 0.01 * (get_weight() - 5670);
+    }
+    float totalFuelConsumption = fuelConsumptionPerMinute * minutes;
+
+    if (get_fuel() - totalFuelConsumption >= 20) {
+        set_fuel(get_fuel() - totalFuelConsumption);
+        set_numberOfFlights(get_numberOfFlights() + 1);
+    }
+}
