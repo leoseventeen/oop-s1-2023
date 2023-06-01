c33b246d4eb0264c3d551e9204f216a1e84b961c
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-06-01 11:18:22 +0930

    main-2.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,19 +1,24 @@
 #include "Trap.h"
 #include "Character.h"
+#include "Utils.h"
 #include <iostream>
 
 int main() {
-    Trap trap(5, 5);
-    Character character(1, 2);
+    Trap trap(3, 3);
+    Character character(1, 1);
 
-    character.move(4, 3);
+    std::cout << "Trap position: " << std::get<0>(trap.getPos()) << ", " << std::get<1>(trap.getPos()) << "\n";
+    std::cout << "Character position: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << "\n";
+
+    character.move(2, 2);
+    std::cout << "Character moved to: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << "\n";
+
+    double distance = Utils::calculateDistance(trap.getPos(), character.getPos());
+    std::cout << "Distance between trap and character: " << distance << "\n";
 
     trap.apply(character);
-
-    std::cout << "Trap Position: " << std::get<0>(trap.getPos()) << ", " << std::get<1>(trap.getPos()) << std::endl;
-    std::cout << "Trap Active: " << (trap.isActive() ? "Yes" : "No") << std::endl;
-    std::cout << "Character Position: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << std::endl;
-    std::cout << "Character Type: " << character.getType() << std::endl;
+    std::cout << "Character's new type (should be 'T' if trap is activated): " << character.getType() << "\n";
+    std::cout << "Is trap still active?: " << (trap.isActive() ? "Yes" : "No") << "\n";
 
     return 0;
 }
