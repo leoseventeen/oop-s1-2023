c5070cb7076ff70aa37b608b71bf0567b6c81929
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-06-01 11:27:57 +0930

    main-2.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,24 +1,21 @@
 #include "Trap.h"
 #include "Character.h"
-#include "Utils.h"
 #include <iostream>
 
 int main() {
-    Trap trap(3, 3);
-    Character character(1, 1);
+    Trap trap(5, 5);
+    Character character(4, 4);
+    character.move(1, 1);
 
-    std::cout << "Trap position: " << std::get<0>(trap.getPos()) << ", " << std::get<1>(trap.getPos()) << "\n";
-    std::cout << "Character position: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << "\n";
+    std::cout << "Trap position: " << std::get<0>(trap.getPos()) << ", " << std::get<1>(trap.getPos()) << std::endl;
+    std::cout << "Character position: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << std::endl;
 
-    character.move(2, 2);
-    std::cout << "Character moved to: " << std::get<0>(character.getPos()) << ", " << std::get<1>(character.getPos()) << "\n";
+    if(trap.isActive() && trap.getPos() == character.getPos()) {
+        trap.apply(character);
+        std::cout << "Trap has been activated!" << std::endl;
+    }
 
-    double distance = Utils::calculateDistance(trap.getPos(), character.getPos());
-    std::cout << "Distance between trap and character: " << distance << "\n";
-
-    trap.apply(character);
-    std::cout << "Character's new type (should be 'T' if trap is activated): " << character.getType() << "\n";
-    std::cout << "Is trap still active?: " << (trap.isActive() ? "Yes" : "No") << "\n";
+    std::cout << "Character type: " << character.getType() << std::endl;
 
     return 0;
 }
