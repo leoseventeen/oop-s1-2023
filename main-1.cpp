975c6e432f8f8e4005252e9546365fdcd079a051
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-06-01 11:26:48 +0930

    main-1.cpp

--- main-1.cpp
+++ main-1.cpp
@@ -4,20 +4,13 @@
 #include <iostream>
 
 int main() {
-    Cell cell(1, 2, 'A');
+    Cell c(1, 2, 'A');
+    std::cout << "Cell position: " << std::get<0>(c.getPos()) << ", " << std::get<1>(c.getPos()) << std::endl;
+    std::cout << "Cell type: " << c.getType() << std::endl;
 
-    // Test Cell
-    std::cout << "Position: " << std::get<0>(cell.getPos()) << ", " << std::get<1>(cell.getPos()) << std::endl;
-    std::cout << "Type: " << cell.getType() << std::endl;
-
-    // Test Utils
     auto pos = Utils::generateRandomPos(10, 10);
-    std::cout << "Random Position: " << std::get<0>(pos) << ", " << std::get<1>(pos) << std::endl;
-
-    std::tuple<int, int> pos1 = std::make_tuple(1, 2);
-    std::tuple<int, int> pos2 = std::make_tuple(4, 6);
-    double distance = Utils::calculateDistance(pos1, pos2);
-    std::cout << "Distance: " << distance << std::endl;
+    std::cout << "Random position: " << std::get<0>(pos) << ", " << std::get<1>(pos) << std::endl;
+    std::cout << "Distance between cell and random position: " << Utils::calculateDistance(c.getPos(), pos) << std::endl;
 
     return 0;
 }
